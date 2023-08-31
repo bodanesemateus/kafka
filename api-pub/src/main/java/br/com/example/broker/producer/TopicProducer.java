30c7059d74815071a6b657e403c2af2e1e797777
mateus-bodanese <mateus-bodanese@auroracoop.com.br>
2023-08-31 13:44:03 -0300

    adiconado docker e apis

---
+++ api-pub/src/main/java/br/com/example/broker/producer/TopicProducer.java
@@ -0,0 +1,23 @@
+package br.com.example.broker.producer;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class TopicProducer {
+
+    @Value("${topic.name.producer}")
+    private String topicName;
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void send(String message){
+        kafkaTemplate.send(topicName, message);
+    }
+
+}
